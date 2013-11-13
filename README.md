52c698dc1211ff7bec4f5647e0c004e706443777
Jon Featherstone <jfeatherstone@frontierstrategygroup.com>
2013-11-12 21:07:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+school-db: a simple php based school database
+==============================================
+
+This is a simple web application that some of my class members will be contributing to in conjunction with our class.
+http://jonfeatherstone.com/education
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	// main include
+	require('main_include.php');
+
+
+?>
+<html>
+	<head>
+		<script src="/lib/jquery/jquery.min.js"></script>
+		<script src="/lib/underscore/underscore-min.js"></script>
+		<script src="/lib/bootstrap/dist/js/bootstrap.min.js"></script>
+		<link href="/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"></link>
+	</head>
+	<body>
+		<!-- login form here -->
+	</body>
+</html>
