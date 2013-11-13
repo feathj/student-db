dac1820e2ffb6528374d97b18d3aa1e706ca2bb9
Jon Featherstone <jfeatherstone@frontierstrategygroup.com>
2013-11-12 21:30:37 -0500

    relative paths to allow running within sites folder

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 ?>
 <html>
 	<head>
-		<script src="/lib/jquery/jquery.min.js"></script>
-		<script src="/lib/underscore/underscore-min.js"></script>
-		<script src="/lib/bootstrap/dist/js/bootstrap.min.js"></script>
-		<link href="/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"></link>
+		<script src="./lib/jquery/jquery.min.js"></script>
+		<script src="./lib/underscore/underscore-min.js"></script>
+		<script src="./lib/bootstrap/dist/js/bootstrap.min.js"></script>
+		<link href="./lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"></link>
 	</head>
 	<body>
 		<!-- login form here -->
