acf6a5f1567e145e5cb855d37fa035bce00b619c
Tyler Peterson <tylerppp@gmail.com>
2014-05-09 16:36:14 -0600

    Form Building

--- admin.php
+++ admin.php
@@ -27,10 +27,37 @@
 						<div class="col-md-4 col-md-offset-4">
 							<div class="panel panel-default">
 								<div class="panel-heading">
-									<h3 class="panel-title">Admin Page</h3>
+									<h3 class="panel-title">Admin Page - You're Special</h3>
 								</div>
 								<div class="panel-body">
-									<p>Hello World!</p>
+									<h4>Create a New User</h4>
+									<form name="createuser" action="" method="post">
+										First Name: <input type="text" name="firstname"><br>
+										Last Name: <input type="text" name="lastname"><br>
+										E-mail: <input type="email" name="email"><br>
+										Password: <input type="password" name="password"><br>
+										<input type="submit" value="Submit">
+									</form>
+									<h4>Update an Existing User</h4>
+									<form name="updateuser" action="" method="post">
+										<select name="users">
+											<?php
+												//fetch possible list of users from db and display in dropdown menu
+												$stmt = $db->prepare('SELECT id, first_name, last_name FROM user');
+												$stmt->execute();
+												$stmt->bind_result($id, $first_name, $last_name);
+												while ($stmt->fetch()) {
+													echo '<option value="' . $id . '">' . $first_name . ' ' . $last_name . '</option>';
+												}
+												$stmt->close();
+											?>
+										</select><br>
+										First Name: <input type="text" name="firstname"><br>
+										Last Name: <input type="text" name="lastname"><br>
+										E-mail: <input type="email" name="email"><br>
+										Password: <input type="password" name="password"><br>
+										<input type="submit" value="Submit">
+									</form>
 								</div>
 							</div>
 						</div>
@@ -40,9 +67,6 @@
 			</body>
 		</html>
 		<?php
-		//code that would allow them to create a user
-		//code that would allow them to update an existing user (including their password)
-
 	}
 	else {
 		//display error message
