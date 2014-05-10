8d2fb7d1fc825402028b9668222c04def2e70ebd
Tyler Peterson <tylerppp@gmail.com>
2014-05-10 11:41:32 -0600

    Put forms in html tables

--- admin.php
+++ admin.php
@@ -27,36 +27,111 @@
 						<div class="col-md-4 col-md-offset-4">
 							<div class="panel panel-default">
 								<div class="panel-heading">
-									<h3 class="panel-title">Admin Page - You're Special</h3>
+									<h2 class="panel-title">Admin Page - You're Special</h2>
 								</div>
 								<div class="panel-body">
-									<h4>Create a New User</h4>
-									<form name="createuser" action="" method="post">
-										First Name: <input type="text" name="firstname"><br>
-										Last Name: <input type="text" name="lastname"><br>
-										E-mail: <input type="email" name="email"><br>
-										Password: <input type="password" name="password"><br>
-										<input type="submit" value="Submit">
+									<h3>Create a New User</h3>
+									<form name="createuser" method="post">
+										<table>
+											<tr>
+												<th>
+													<label for="cfirstname">First Name:</label>
+												</th>
+												<td>
+													<input type="text" name="cfirstname" id="cfirstname">
+												</td>
+											</tr>
+											<tr>
+												<th>
+													<label for="clastname">Last Name:</label>
+												</th>
+												<td>
+													<input type="text" name="clastname" id="clastname">
+												</td>
+											</tr>
+											<tr>
+												<th>
+													<label for="cemail">E-mail:</label>
+												</th>
+												<td>
+													<input type="email" name="cemail" id="cemail">
+												</td>
+											</tr>
+											<tr>
+												<th>
+													<label for="cpassword">Password:</label>
+												</th>
+												<td>
+													<input type="password" name="cpassword" id="cpassword">
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<input type="submit" value="Submit">
+												</td>
+											</tr>
+										</table>
 									</form>
-									<h4>Update an Existing User</h4>
-									<form name="updateuser" action="" method="post">
-										<select name="users">
-											<?php
-												//fetch possible list of users from db and display in dropdown menu
-												$stmt = $db->prepare('SELECT id, first_name, last_name FROM user');
-												$stmt->execute();
-												$stmt->bind_result($id, $first_name, $last_name);
-												while ($stmt->fetch()) {
-													echo '<option value="' . $id . '">' . $first_name . ' ' . $last_name . '</option>';
-												}
-												$stmt->close();
-											?>
-										</select><br>
-										First Name: <input type="text" name="firstname"><br>
-										Last Name: <input type="text" name="lastname"><br>
-										E-mail: <input type="email" name="email"><br>
-										Password: <input type="password" name="password"><br>
-										<input type="submit" value="Submit">
+									<h3>Update an Existing User</h3>
+									<form name="updateuser" method="post">
+										<table>
+											<tr>
+												<th>
+													<label for="select">Select a user:</label>
+												</th>
+												<td>
+													<select name="users">
+														<?php
+															//fetch possible list of users from db and display in dropdown menu
+															$stmt = $db->prepare('SELECT id, first_name, last_name FROM user');
+															$stmt->execute();
+															$stmt->bind_result($id, $first_name, $last_name);
+															while ($stmt->fetch()) {
+																echo '<option value="' . $id . '">' . $first_name . ' ' . $last_name . '</option>';
+															}
+															$stmt->close();
+														?>
+													</select>
+												</td>
+											</tr>
+											<tr>
+												<th>
+													<label for="firstname">First Name:</label>
+												</th>
+												<td>
+													<input type="text" name="firstname" id="firstname">
+												</td>
+											</tr>
+											<tr>
+												<th>
+													<label for="lastname">Last Name:</label>
+												</th>
+												<td>
+													<input type="text" name="lastname" id="lastname">
+												</td>
+											</tr>
+											<tr>
+												<th>
+													<label for="email">E-mail:</label>
+												</th>
+												<td>
+													<input type="email" name="email" id="email">
+												</td>
+											</tr>
+											<tr>
+												<th>
+													<label for="password">Password:</label>
+												</th>
+												<td>
+													<input type="password" name="password" id="password">
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<input type="submit" value="Submit">
+												</td>
+											</tr>
+										</table>
 									</form>
 								</div>
 							</div>
