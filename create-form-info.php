8056ad7b8371c4986192ecac1ce352112e3ff372
Tyler Peterson <tylerppp@gmail.com>
2014-05-15 08:21:10 -0600

    Forms now updating the DB for FN, LN and Email

--- create-form-info.php
+++ create-form-info.php
@@ -1,9 +1,13 @@
 <?php
+require('main_include.php');
 
 $cfirstname = $_POST["cfirstname"];
 $clastname = $_POST["clastname"];
 $cemail = $_POST["cemail"];
 $cpassword = $_POST["cpassword"];
+
+mysqli_query($db,"INSERT INTO user (email, first_name, last_name) VALUES ('$cemail', '$cfirstname', '$clastname')");
+mysqli_close($db);
 
 header("Location: thankyou.php");
 
--- update-form-info.php
+++ update-form-info.php
@@ -1,9 +1,14 @@
 <?php
+require('main_include.php');
 
+$id = $_POST["users"];
 $firstname = $_POST["firstname"];
 $lastname = $_POST["lastname"];
 $email = $_POST["email"];
 $password = $_POST["password"];
+
+mysqli_query($db,"UPDATE user SET email='$email', first_name='$firstname', last_name='$lastname' WHERE id=$id");
+mysqli_close($db);
 
 header("Location: thankyou.php");
 
