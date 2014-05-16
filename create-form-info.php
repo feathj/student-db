8cc501b98819eddfd538cdf468fd6918c026371d
Tyler Peterson <tylerppp@gmail.com>
2014-05-16 14:41:06 -0600

    Modified create form to insert admin value to DB

--- create-form-info.php
+++ create-form-info.php
@@ -5,8 +5,9 @@ $cfirstname = $_POST["cfirstname"];
 $clastname = $_POST["clastname"];
 $cemail = $_POST["cemail"];
 $cpassword = sha1($_POST["cpassword"]);
+$cadmin = $_POST["cradio"];
 
-mysqli_query($db,"INSERT INTO user (email, first_name, last_name, encrypted_password) VALUES ('$cemail', '$cfirstname', '$clastname', '$cpassword')");
+mysqli_query($db,"INSERT INTO user (email, first_name, last_name, encrypted_password, is_admin) VALUES ('$cemail', '$cfirstname', '$clastname', '$cpassword', $cadmin)");
 mysqli_close($db);
 
 header("Location: thankyou.php");
