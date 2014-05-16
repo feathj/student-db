d83b81d3badf9fe40e02dbdd75774fb00e2d274c
Tyler Peterson <tylerppp@gmail.com>
2014-05-16 14:07:54 -0600

    Encrypting passwords before they are entered into the DB

--- create-form-info.php
+++ create-form-info.php
@@ -4,9 +4,9 @@ require('main_include.php');
 $cfirstname = $_POST["cfirstname"];
 $clastname = $_POST["clastname"];
 $cemail = $_POST["cemail"];
-$cpassword = $_POST["cpassword"];
+$cpassword = sha1($_POST["cpassword"]);
 
-mysqli_query($db,"INSERT INTO user (email, first_name, last_name) VALUES ('$cemail', '$cfirstname', '$clastname')");
+mysqli_query($db,"INSERT INTO user (email, first_name, last_name, encrypted_password) VALUES ('$cemail', '$cfirstname', '$clastname', '$cpassword')");
 mysqli_close($db);
 
 header("Location: thankyou.php");
--- update-form-info.php
+++ update-form-info.php
@@ -5,9 +5,9 @@ $id = $_POST["users"];
 $firstname = $_POST["firstname"];
 $lastname = $_POST["lastname"];
 $email = $_POST["email"];
-$password = $_POST["password"];
+$password = sha1($_POST["password"]);
 
-mysqli_query($db,"UPDATE user SET email='$email', first_name='$firstname', last_name='$lastname' WHERE id=$id");
+mysqli_query($db,"UPDATE user SET email='$email', first_name='$firstname', last_name='$lastname', encrypted_password='$password' WHERE id=$id");
 mysqli_close($db);
 
 header("Location: thankyou.php");
