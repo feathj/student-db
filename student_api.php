bf67f4e8129bf48bef7c4ba5b3d126bea4f2817b
Tyler Peterson <tylerppp@gmail.com>
2014-05-30 15:34:02 -0600

    student_api.php takes info from student table and echos JSON

---
+++ student_api.php
@@ -0,0 +1,18 @@
+<?php
+require('main_include.php');
+
+$result = mysqli_query($db, "SELECT id, first_name, last_name FROM student");
+
+$json = array();
+
+if (mysqli_num_rows($result)) {
+	while ($row = mysqli_fetch_row($result)) {
+		$json['student'][] = $row;
+	}
+}
+
+mysqli_close($db);
+
+echo json_encode($json);
+
+?>
