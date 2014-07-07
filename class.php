d76d215aedde3cb7569e40395b1b71fa98773484
Tyler Peterson <tylerppp@gmail.com>
2014-07-07 13:40:50 -0600

    Trying to get teachers and students preselected and checked on the form if they already belong to the class

--- class.php
+++ class.php
@@ -6,14 +6,22 @@ require('main-nav.php');
 <?php
 $class_id = intval($_GET['id']);
 
-$stmt = $db->prepare("SELECT title, credit_hours FROM class WHERE id = '".$class_id."'");
+$stmt = $db->prepare("SELECT title, credit_hours, teacher_id FROM class WHERE id = ?");
+$stmt->bind_param('i', $class_id);
 $stmt->execute();
-$stmt->bind_result($title, $credit_hours);
+$stmt->bind_result($title, $credit_hours, $teacher_id);
 $stmt->fetch();
 $stmt->close();
 
+$stmtI = $db->prepare('SELECT student_id FROM student_class WHERE class_id=?');
+$stmtI->bind_param('i', $class_id);
+$stmtI->execute();
+$stmtI->bind_result($sc_id);
+$stmtI->fetch();
+$stmtI->close();
+
 echo "<html><div class='container'><div class='col-md-4 col-md-offset-0'><ul class='list-group details'><li class='list-group-item'>".$title."</li>";
-echo "<li class='list-group-item'>Credit Hours: ".$credit_hours."</li></ul>";
+echo "<li class='list-group-item'>Credit Hours: ".$credit_hours.'.0'."</li></ul>";
 ?>
 <a href="classes.php"><div class="glyphicon glyphicon-arrow-left"></div>&nbsp;Back to All Classes</a>
 		</div>
@@ -33,13 +41,17 @@ echo "<li class='list-group-item'>Credit Hours: ".$credit_hours."</li></ul>";
 									<select name="teachers" id="teachers">
 										<?php
 											//fetch possible list of teachers from db and display in dropdown menu
-											$stmtI = $db->prepare('SELECT id, first_name, last_name FROM teacher');
-											$stmtI->execute();
-											$stmtI->bind_result($id, $first_name, $last_name);
-											while ($stmtI->fetch()) {
-												echo '<option value="' . $id . '">' . $first_name . ' ' . $last_name . '</option>';
-											}
-											$stmtI->close();
+											$stmtII = $db->prepare('SELECT id, first_name, last_name FROM teacher');
+											$stmtII->execute();
+											$stmtII->bind_result($id, $first_name, $last_name);
+											while ($stmtII->fetch()) {
+												if ($id == $teacher_id) {
+													echo '<option value="' . $id . '" selected>' . $first_name . ' ' . $last_name . '</option>';
+												} else {
+													echo '<option value="' . $id . '">' . $first_name . ' ' . $last_name . '</option>';													
+												}
+											}	
+											$stmtII->close();
 										?>
 									</select>
 								</td>
@@ -50,13 +62,17 @@ echo "<li class='list-group-item'>Credit Hours: ".$credit_hours."</li></ul>";
 								</th>
 								<td>
 									<?php
-										$stmtII = $db->prepare('SELECT id, first_name, last_name FROM student');
-										$stmtII->execute();
-										$stmtII->bind_result($sid, $sfirst_name, $slast_name);
-										while ($stmtII->fetch()) {
-											echo '<input type="checkbox" name="students[]" value="'.$sid.'">'.$sfirst_name.' '.$slast_name.'<br>';
+										$stmtIII = $db->prepare('SELECT id, first_name, last_name FROM student');
+										$stmtIII->execute();
+										$stmtIII->bind_result($sid, $sfirst_name, $slast_name);
+										while ($stmtIII->fetch()) {
+											if ($sid == $sc_id) {
+												echo '<input type="checkbox" checked="true" name="students[]" value="'.$sid.'">'.$sfirst_name.' '.$slast_name.'<br>';
+											} else {
+												echo '<input type="checkbox" name="students[]" value="'.$sid.'">'.$sfirst_name.' '.$slast_name.'<br>';												
+											}
 										}
-										$stmtII->close();
+										$stmtIII->close();
 									?>
 								</td>
 							</tr>
